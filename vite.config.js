691d96ff3fd1c01603275e54d8d72771ab4de429
jjyeong2103 <134499661+jjyeong2103@users.noreply.github.com>
2025-06-23 16:16:32 +0900

    두번째 페이지 수정

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        chatbot: resolve(__dirname, 'chatbotmain.html'),
+        chatbot: resolve(__dirname, 'chatbot.html'),
       },
     },
   },
